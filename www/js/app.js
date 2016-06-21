24c77090c38105fd930f4b4b838122c97ab103ee
stbyrne <stuart.byrne@hmhco.com>
2016-06-21 14:21:49 +0100

    commit_work_31

--- www/js/app.js
+++ www/js/app.js
@@ -1,56 +1,57 @@
+//Ini
+
 var question = {
-    'stimulus': ''
+    'stimulus': 'Question_should_appear_here!'
 };
 
-var handleOpenURL = function(url) {
-    
-    var stimulus = url.split('='),
-        tempQuestion = stimulus[1];
 
-tempQuestion.forEach(function(str){
+var handleOpenURL = function (url) {
 
-str = str.replace("_"," ");
+    var holder = url.split("="),
+        strArray = holder[1].split("_"),
+        le = strArray.length;
 
-});
-    
-    question.stimulus = tempQuestion;
-    
-}; 
-        
+    if (le > 1) {
+        tempQuestion = strArray.slice(0, le).join(" ");
+        question.stimulus = tempQuestion;
+    }
+
+};
+
 
 angular.module('recorder', ['ionic', 'recorder.controllers', 'recorder.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
+    $stateProvider
 
-  .state('home', {
-    url: '/home',
-    controller: 'HomeCtrl',
-    templateUrl: 'templates/home.html'
-  })
-	.state('new', {
-		url:'/new',
-		controller: 'RecordCtrl',
-		templateUrl: 'templates/new.html'
-	});
+        .state('home', {
+            url: '/home',
+            controller: 'HomeCtrl',
+            templateUrl: 'templates/home.html'
+        })
+        .state('new', {
+            url: '/new',
+            controller: 'RecordCtrl',
+            templateUrl: 'templates/new.html'
+        });
 
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/home');
 
 });
